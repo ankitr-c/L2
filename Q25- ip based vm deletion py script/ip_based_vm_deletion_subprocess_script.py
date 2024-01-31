e85b209d47bb7eea2ce56b5094a5813c81c70b17
Ankit <ankit.raut@coditas.com>
2024-01-31 15:00:59 +0530

    Q1,Q2,Q24

--- Q25- ip based vm deletion py script/ip_based_vm_deletion_subprocess_script.py
+++ Q25- ip based vm deletion py script/ip_based_vm_deletion_subprocess_script.py
@@ -1,18 +1,40 @@
-# gcloud compute instances list --filter="networkInterfaces.accessConfigs.natIP=10.138.0.2 OR networkInterfaces.networkIP=10.138.0.2"
-# gcloud compute instances delete my-vm --zone=us-central1-a
+# # gcloud compute instances list --filter="networkInterfaces.accessConfigs.natIP=10.138.0.2 OR networkInterfaces.networkIP=10.138.0.2"
+# # gcloud compute instances delete my-vm --zone=us-central1-a
+# import subprocess
+
+# def delete_vm_by_ip(ip_address):
+#     try:
+#         op = subprocess.check_output(['gcloud.cmd', 'compute', 'instances', 'list', '--filter', f'networkInterfaces.accessConfigs.natIP={ip_address} OR networkInterfaces.networkIP={ip_address}'], text=True)
+#         vm_name=op.split('\n')[0].split(":")[1].strip()
+#         print(vm_name)
+#         command=['gcloud.cmd', 'compute', 'instances', 'delete', vm_name]
+#         ans=subprocess.run(command, check=True)
+#         print(ans)
+#         print("Vm: ",vm_name,"Deleted...!")
+#     except subprocess.CalledProcessError as e:
+#         print(f"Error: {e}")
+
+# ip_address = "10.138.0.2"
+# delete_vm_by_ip(ip_address)
 import subprocess
 
 def delete_vm_by_ip(ip_address):
     try:
         op = subprocess.check_output(['gcloud.cmd', 'compute', 'instances', 'list', '--filter', f'networkInterfaces.accessConfigs.natIP={ip_address} OR networkInterfaces.networkIP={ip_address}'], text=True)
+        print(op)
         vm_name=op.split('\n')[0].split(":")[1].strip()
+        zone=op.split('\n')[1].split(":")[1].strip()
         print(vm_name)
         command=['gcloud.cmd', 'compute', 'instances', 'delete', vm_name]
+        # command=['gcloud.cmd', 'compute', 'instances', 'delete', vm_name,'--zone',zone]
+        
         ans=subprocess.run(command, check=True)
         print(ans)
         print("Vm: ",vm_name,"Deleted...!")
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
 
-ip_address = "10.138.0.2"
+# ip_address = "10.138.0.2"
+ip_address=input('Enter IP:  ')
 delete_vm_by_ip(ip_address)
+
